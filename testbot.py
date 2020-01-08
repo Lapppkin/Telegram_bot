44b15021b20a66fd2acd7c7507d2913cebefaa3d
LapkinLab <lapppkin@yahoo.com>
2020-01-08 10:14:20 +0300

    start works

--- testbot.py
+++ testbot.py
@@ -2,6 +2,21 @@ import telebot
 bot = telebot.TeleBot('1054775144:AAGR0Pu07k2Ql7VdhleiKL1bl79J6keAEfA')
 
 
-@bot.add_message_handler(content_types=['text', 'document', 'audio'])
-def get_text_messages(message):
 
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'Привет':
+        bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
+    elif message.text == '/help':
+        bot.send_message(message.from_user.id, 'Напиши привет')
+    else:
+        bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help.')
+
+
+
+bot.polling(none_stop=True, interval=0)
