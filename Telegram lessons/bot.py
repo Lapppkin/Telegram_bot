24a27034f72efd711491ba5821602971cd823c05
LapkinLab <lapppkin@yahoo.com>
2020-01-08 11:30:38 +0300

    start works

---
+++ Telegram lessons/bot.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import config
+import telebot
+from telebot import apihelper
+
+bot = telebot.TeleBot(config.token)
+
+
+
+
---
+++ Telegram lessons/config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Токен создаем в @botfather :)
+token = '1054775144:AAGR0Pu07k2Ql7VdhleiKL1bl79J6keAEfA'
