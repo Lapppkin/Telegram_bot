a9a5edd5c60f902edd73b87b3caa4e599dc87c07
LapkinLab <lapppkin@yahoo.com>
2020-01-08 11:21:13 +0300

    start works

--- testbot.py
+++ testbot.py
@@ -14,18 +14,17 @@ bot = telebot.TeleBot('1054775144:AAGR0Pu07k2Ql7VdhleiKL1bl79J6keAEfA')
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-
     if message.text == 'Привет':
         bot.send_message(message.from_user.id, 'Привет, чем я могу тебе помочь?')
     elif message.text == '/help':
         bot.send_message(message.from_user.id, 'Напиши привет')
     elif message.text == "Кто ты?":
-        bot.send_message(message.from_user.id, "Я тестовый чатбот для учебного примера.")
-    elif message.text == "Как тебя зовут?":
-        bot.send_message(message.from_user.id, "Меня зовут MyFirstTestBot.")
-    elif message.text == "Что ты умеешь?":
-        bot.send_message(message.from_user.id,
-                         "Я умею отвечать на несколько простых вопросов - кто я, как меня зовут и что я умею делать.")
+        bot.send_message(message.from_user.id, 'Я тестовый чатбот для учебного примера.')
+    elif message.text == 'Как тебя зовут?':
+        bot.send_message(message.from_user.id, 'Меня зовут MyFirstTestBot.')
+    elif message.text == 'Что ты умеешь?':
+        bot.send_message(message.from_user.id, 'Я умею отвечать на несколько простых вопросов' +
+                         '- кто я, как меня зовут и что я умею делать.')
     else:
         bot.send_message(message.from_user.id, 'Я тебя не понимаю. Напиши /help.')
 
